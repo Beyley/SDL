da5026ca2fb75d2e91cc3681700e8ba27f06b965
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-12-23 19:41:29 -0800

    gpu: Fix uninitialized variable usage in OpenXR physical device init

--- src/gpu/vulkan/SDL_gpu_vulkan.c
+++ src/gpu/vulkan/SDL_gpu_vulkan.c
@@ -11787,6 +11787,9 @@ static Uint8 VULKAN_INTERNAL_DeterminePhysicalDevice(VulkanRenderer *renderer)
             SDL_stack_free(physicalDeviceExtensions);
             return 0;
         }
+
+        SDL_stack_free(physicalDevices);
+        SDL_stack_free(physicalDeviceExtensions);
     }
 
     renderer->physicalDeviceProperties.sType =
@@ -11814,8 +11817,6 @@ static Uint8 VULKAN_INTERNAL_DeterminePhysicalDevice(VulkanRenderer *renderer)
         renderer->physicalDevice,
         &renderer->memoryProperties);
 
-    SDL_stack_free(physicalDevices);
-    SDL_stack_free(physicalDeviceExtensions);
     return 1;
 }
 
